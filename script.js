e3f1ca9dc62a895e257dbccf88499082b3510ffc
RahulDani <rahuldani321@gmail.com>
2024-09-12 23:22:19 +0530

    desinging song player

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 console.log("lets write js")
-async function main() {
-    let a =await fetch("http://127.0.0.1:3000/songs/");
+async function getsongs(linkforsongs) {
+    let a =await fetch(linkforsongs);
     let response = await a.text();
-    console.log(response);
+    // console.log(response);
+    let div=document.createElement("div");
+    div.innerHTML=response;
+    let as=div.getElementsByTagName("a");
+    // console.log(as);
+    let songs=[];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href.split("/songs/")[1]); // used splt to start from songs see the element how is that its a link 
+        }
+        
+    }      
+
+    // console.log(songs);
+    return songs;
     
     
 }
 
-main()
+
+async function main(){
+
+    let songs=await getsongs("http://127.0.0.1:3000/songs/")
+    console.log(songs);
+
+    let songsUL= document.querySelector(".songList").getElementsByTagName("ul")[0];
+
+
+    // for (const Song of songs) {
+    //     songsUL.innerHTML = songsUL.innerHTML + `<li> ${Song} </li>`;
+    // }  before to remove unwaanted things in songs like %20
+
+    // after
+
+    for (const Song of songs) {
+        songsUL.innerHTML = songsUL.innerHTML + `<li> ${Song.replaceAll("%20"," ")} </li>`;
+    }
+
+    var audio = new Audio(songs[1]);
+    // audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration,audio.currentTime);
+        
+});
+
+    
+}
+
+main();
